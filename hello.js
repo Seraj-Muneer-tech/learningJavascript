f894270db3c70a9e02be123b16e82a3b43aa41c2
seraj-muneer-tech <seraj.munir1@gmail.com>
2021-05-29 22:50:34 +0530

    heading's font size changes using javascript

--- hello.js
+++ hello.js
@@ -1,8 +1,10 @@
 function myFunc(click){
     if(click=='hide'){
         document.getElementById("p1").style.display="none"
+        document.getElementById("heading").style.fontSize="16px"
     }
     else if(click=='show'){
         document.getElementById("p1").style.display="block"
+        document.getElementById("heading").style.fontSize="60px"
     }
 }
