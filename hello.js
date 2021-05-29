350d84802fa20f918ccfa89dc96fd739e8303199
seraj-muneer-tech <seraj.munir1@gmail.com>
2021-05-29 21:39:40 +0530

    First commit in javascript learning series.

---
+++ hello.js
@@ -0,0 +1,8 @@
+function myFunc(click){
+    if(click=='hide'){
+        document.getElementById("p1").style.display="none"
+    }
+    else if(click=='show'){
+        document.getElementById("p1").style.display="block"
+    }
+}
